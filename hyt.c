62dc0a96a3d550898f3d5aead72c69ee5c82fdd7
Iyundhu Kennedy <kennedyiyundhu@gmail.com>
2025-04-04 14:05:47 +0300

    C program

---
+++ hyt.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int course_one,course_two,course_three,course_four;
+	printf("course_one: ");
+	scanf("%d", &course_one);
+	printf("course_two: ");
+	scanf("%d", &course_two);
+	printf("course_three: ");
+	scanf("%d", &course_three);
+	printf("course_four: ");
+	scanf("%d", &course_four);
+//	printf("course_one+course_two+course_three+course_three = %d");
+	
+}
