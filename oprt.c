cc88ecbbc80630269bd73d69d31192e2b2336cec
Iyundhu Kennedy <kennedyiyundhu@gmail.com>
2025-04-04 14:08:44 +0300

    Add files via upload

---
+++ oprt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	int x;
+	int y;
+	
+	printf("value of x;")
+	scanf("%d\n",&x)
+	printfv("value of y;")
+		scanf("%d\n",&y)
+	
+	printf("the sum of x + y ",x +y);
+	printf("the sum of x - y ",x-y);
+	printf("the sum of x * y ",x *y);
+	printf("the sum of x / y ",x /y);
+	
+	
+}
