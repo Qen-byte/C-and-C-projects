8dfc1e9e15257077113eeedb0d2a9ad16ffbe0af
Iyundhu Kennedy <kennedyiyundhu@gmail.com>
2025-04-04 14:02:40 +0300

    create cki

---
+++ cki.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+//	int course_one,course_two,course_three,course_four;
+//	printf("course_one: ");
+////	scanf("%d", &course_one);
+	//printf("course_two: ");
+	//scanf("%d", &course_two);
+	//printf("course_three: ");
+	//scanf("%d", &course_three);
+	//printf("course_four: ");
+	//scanf("%d", &course_four);
+	//return 0;
+	int a,b;
+	printf("enter a:");
+	scanf("%d" , &a);
+	printf("enter b:");
+	scan("%d", &b);
+}
